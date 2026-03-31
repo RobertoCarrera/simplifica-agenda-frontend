a41871f039386b519f2add04c6aaeae3e7712152
RobertoCarrera <robertocarrera93@gmail.com>
2026-04-01 01:36:08 +0200

    chore(security): untrack environment files with secrets from git

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  bffBaseUrl: "https://ufutyjbqfjrlzkprvyvs.supabase.co/functions/v1/booking-public",
-  turnstileSiteKey: "0x4AAAAAACsJYKYeUGjcsl-L0FlP15fDjd8",
-  clientId: "simplifica-agenda-frontend",
-  supabaseAnonKey: "SUPABASE_ANON_KEY_REDACTED",
-  bookingApiKey: "BOOKING_API_KEY_REDACTED",
-};
