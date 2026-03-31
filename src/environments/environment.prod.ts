650f065f50ad00b88250659961f03069b6c58996
RobertoCarrera <robertocarrera93@gmail.com>
2026-03-31 04:18:41 +0200

    fix_clientid

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   bffBaseUrl:
     "https://ufutyjbqfjrlzkprvyvs.supabase.co/functions/v1/booking-public",
   turnstileSiteKey: "0x4AAAAAAAJ6h2j2U6bK9a3b",
-  clientId: "simplify-agenda-frontend",
+  clientId: "simplifica-agenda-frontend",
 };
