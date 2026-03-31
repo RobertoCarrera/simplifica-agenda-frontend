4185a86004d90e296a0d9d47ea3fae8d3f76d77e
RobertoCarrera <robertocarrera93@gmail.com>
2026-04-01 01:18:04 +0200

    chore(env): update environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  bffBaseUrl:
-    "https://ufutyjbqfjrlzkprvyvs.supabase.co/functions/v1/booking-public",
-  turnstileSiteKey: "0x4AAAAAAAJ6h2j2U6bK9a3b",
+  bffBaseUrl: "https://ufutyjbqfjrlzkprvyvs.supabase.co/functions/v1/booking-public",
+  turnstileSiteKey: "0x4AAAAAACsJYKYeUGjcsl-L0FlP15fDjd8",
   clientId: "simplifica-agenda-frontend",
+  supabaseAnonKey: "SUPABASE_ANON_KEY_REDACTED",
+  bookingApiKey: "BOOKING_API_KEY_REDACTED",
 };
