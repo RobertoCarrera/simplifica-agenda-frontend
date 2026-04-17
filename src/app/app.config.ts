b13870269ca034a451e675b52eaa94d49fc08bc3
Sincronia Pentester <sincronia.pentester@gmail.com>
2026-04-17 18:41:25 +0000

    feat(pwa): add progressive web app support with offline caching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
 import { provideAnimations } from "@angular/platform-browser/animations";
@@ -7,6 +7,7 @@ import { provideTransloco } from "@jsverse/transloco";
 import { TranslocoHttpLoader } from "./transloco-http.loader";
 
 import { routes } from "./app.routes";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,6 +27,9 @@ export const appConfig: ApplicationConfig = {
         },
       },
       loader: TranslocoHttpLoader,
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
